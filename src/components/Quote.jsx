b92d7033cb3c8c2a3115befa7bb1ad8815aecf95
abhi1994j <abhibaban4@gmail.com>
2025-04-13 00:29:15 +0530

    complete quote page

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -111,10 +111,10 @@ const Quote = () => {
 
   return (
     <>
-      <div className="quote min-h-[100vh] w-[100%] flex flex-col gap-4 p-4 ">
+      <div className="quote min-h-[100vh] w-[100%] flex flex-col gap-4 md:p-4 p-2 ">
         {quotes && quotes.length>0 ? quotes.map((ele)=>
-           <div key={ele.id} className="card text-white bg-gray-800 py-6 px-10 rounded-lg m-4">
-          <h1 className="md:text-xl sm:text-lg text-sm font-bold mb-4">{ele.quotes}</h1>
+           <div key={ele.id} className="card text-white bg-gray-800 py-3 md:py-6 px-4 sm:px-6 md:px-10 rounded-lg sm:m-2 md:m-4">
+          <h1 className="md:text-xl sm:text-lg text-sm font-bold mb-2 md:mb-4">{ele.quotes}</h1>
           <p className="text-center text-gray-500 text-sm sm:text-lg font-semibold">{ele.author}</p>
     </div>)
         
